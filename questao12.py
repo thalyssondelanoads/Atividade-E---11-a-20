c7fb547152e502d5747c2c4114edf4d7e118e5ae
thalyssondelanoads <159953846+thalyssondelanoads@users.noreply.github.com>
2024-02-26 23:38:23 -0300

    Create questao12.py

---
+++ questao12.py
@@ -0,0 +1,16 @@
+def impar_par():
+    if num % 2 == 0:
+        print('O Número é Par')
+    else:
+        print('O Número é Impar')
+    
+
+#-----------------------------
+
+print('Impar ou Par')
+print('--------------')
+
+num = int(input('Digite o Número: '))
+print('--------------')
+
+impar_par()
